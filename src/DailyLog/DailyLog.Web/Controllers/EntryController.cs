eedf177b123d4caf4c1f640ec5114f96242ad0b9
bogusz <bogusz@github.com>
2013-01-26 21:24:46 +0100

    add project

---
+++ src/DailyLog/DailyLog.Web/Controllers/EntryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DailyLog.Web.Controllers
+{
+    public class EntryController : Controller
+    {        
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello World!";
+
+            return View();
+        }
+    }
+}
---
+++ src/DailyLog/DailyLog.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DailyLog.Web.MvcApplication" Language="C#" %>
